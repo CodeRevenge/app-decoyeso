4d3374835116a539101bafa3b9a09925200f7c78
CodeRevenge <mas.arley2009@hotmail.com>
2020-04-28 23:20:48 -0500

    login page

---
+++ app/Views/Login/login-page.js
@@ -0,0 +1,53 @@
+var LoginViewModel = require("./login-view-model");
+var loginViewModel = new LoginViewModel();
+const dialogs = require("tns-core-modules/ui/dialogs");
+
+function pageLoaded(args) {
+    var page = args.object;
+    page.bindingContext = loginViewModel;
+}
+
+exports.Login = (args) => {
+    const login = args.object;
+    const loginParent = login.parent;
+
+    const nickname = loginParent.getViewById("nickname");
+    const password = loginParent.getViewById("password");
+
+    const conectionLink = "http://ppicucei.000webhostapp.com/decoyeso/login.php";
+
+    var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function () {
+		if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+            const response = JSON.parse(this.responseText);
+			if (response.status === "OK") {
+                console.log("LOGIN");
+                dialogs.alert({
+                    title: "LOGIN",
+                    message: `${response.message}`,
+                    okButtonText: "Ok",
+                })
+			} else if (response.status === "WARNING") {
+                dialogs.alert({
+                    title: "Error",
+                    message: `${response.message}`,
+                    okButtonText: "Ok",
+                })
+            } else {
+                dialogs.alert({
+                    title: "Error",
+                    message: `Sucedio un error inesperado. ${response}`,
+                    okButtonText: "Ok",
+                })
+            }
+
+		}
+	};
+	let URI = encodeURI(`${conectionLink}?nickname=${nickname.text}&password=${password.text}`)
+    console.log(URI);
+	xhttp.open("GET", URI, true);
+	xhttp.send();
+}
+
+exports.pageLoaded = pageLoaded;
---
+++ app/Views/Login/login-view-model.js
@@ -0,0 +1,11 @@
+var observableModule = require("tns-core-modules/data/observable");
+
+function LoginViewModel() {
+  var viewModel = observableModule.fromObject({
+
+  });
+
+  return viewModel;
+}
+
+module.exports = LoginViewModel;
